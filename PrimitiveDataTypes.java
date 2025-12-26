a842d50c8f926785a8d3adb6396212edcfedf405
3332221117 <254G5A0543@srit.ac.in>
2025-12-26 15:42:38 +0530

    Add files via upload

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,21 @@
+class PrimitiveDataTypes{
+byte b;
+short s;
+int i;
+float f;
+long l;
+double d;
+char c;
+boolean bool;
+public static void main(String args[]){
+PrimitiveDataTypes obj = new PrimitiveDataTypes();
+System.out.println("Default byte value:"+obj.b);
+System.out.println("Default short value:"+obj.s);
+System.out.println("Default int value:"+obj.i);
+System.out.println("Default float value:"+obj.f);
+System.out.println("Default long value:"+obj.l);
+System.out.println("Default double value:"+obj.d);
+System.out.println("Default charvalue:"+obj.c);
+System.out.println("Default boolean value:"+obj.bool);
+}
+}
